43bd9b5130134f8e19f6e655b3e7c45dfe15c96d
Talha Kök <talha@talhakoek.com>
2024-04-17 10:02:00 +0000

    Introduced multithearded processing of the MatchDetails reducing the total execution time by 2000 ms and Headers added Platformid and version

--- src/main/java/com/talhakoek/valorant/DataScrollerView.java
+++ src/main/java/com/talhakoek/valorant/DataScrollerView.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.talhakoek.valorant.api.ApiException;
 import com.talhakoek.valorant.models.MapsResponse;
 import com.talhakoek.valorant.models.MatchHistoryResponse;
+import com.talhakoek.valorant.services.MatchDetailsService;
 import com.talhakoek.valorant.services.MatchHistoryService;
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
@@ -22,11 +23,10 @@ import java.net.http.HttpResponse;
 @RequestScoped
 public class DataScrollerView implements Serializable {
 
-    String PUUID="";
-    int start=0;
-    int end=20;
-    String X_Riot_Entitlements_JWT ="eyJraWQiOiJrMSIsImFsZyI6IlJTMjU2In0.eyJlbnRpdGxlbWVudHMiOltdLCJhdF9oYXNoIjoiOTBwWldiWEVyMnhneDFNX3VGZk1NUSIsInN1YiI6IjMxY2RhODMxLTY4YjUtNTkyMy04YzBmLWU2YTkzNmViNDllZSIsImlzcyI6Imh0dHBzOlwvXC9lbnRpdGxlbWVudHMuYXV0aC5yaW90Z2FtZXMuY29tIiwiaWF0IjoxNzEzMDk5NjAzLCJqdGkiOiJFZUp3cDNNYmlaNCJ9.Z47ZXEtYJ-4cCiub78cZQFxjPj0P4XXq6rw8gN7ofNoWMLelDpqf_sIOkdOv1sS1hCFHcWdmrRlLl-zJcxkUq1ugcbzMNi3V0ldZDYYbl9HbN5qt9DPrmI5KKEV6Aew1EQEhw6-9Du8mUcMrEJVW42e0BkNHeKI484cNfGTbGzXlxzb2LV7ypfaePmqV1h5sf-R3cj01xnTQrwJSep5YNkVghkQHsfWX7Yssbs3_2RwttNwAbjyK25oUhrI6Wz85qIfS7BCLwqE-JnV8wew3ErMBXBOkfU4uTaP9qkdQQQ0LP0vvIyKbNTmH75-HzArTuEdfGNscR9DRKxECXG_tHw";
-    String Authorization="eyJraWQiOiJzMSIsImFsZyI6IlJTMjU2In0.eyJwcCI6eyJjIjoiZXUifSwic3ViIjoiMzFjZGE4MzEtNjhiNS01OTIzLThjMGYtZTZhOTM2ZWI0OWVlIiwic2NwIjpbImFjY291bnQiLCJvcGVuaWQiXSwiY2xtIjpbImVtYWlsX3ZlcmlmaWVkIiwib3BlbmlkIiwicHciLCJyZ25fRVVXMSIsInBob25lX251bWJlcl92ZXJpZmllZCIsImxvY2FsZSIsImFjY291bnRfdmVyaWZpZWQiLCJmZWRlcmF0ZWRfaWRlbnRpdHlfZGV0YWlscyIsImZlZGVyYXRlZF9pZGVudGl0eV9wcm92aWRlcnMiLCJhY2N0X2dudCIsImFjY3QiLCJhZ2UiLCJhZmZpbml0eSJdLCJkYXQiOnsicCI6bnVsbCwiciI6IkVVVzEiLCJjIjoiZWMxIiwidSI6Mjk0NDc1NDgzMDA4MzM2MCwibGlkIjoiNEU4UmVzeWRVUFA4WGVXNk8yZlJidyJ9LCJpc3MiOiJodHRwczovL2F1dGgucmlvdGdhbWVzLmNvbSIsInBsdCI6eyJkZXYiOiJ1bmtub3duIiwiaWQiOiJ3aW5kb3dzIn0sImV4cCI6MTcxMzEwMzIwMiwiaWF0IjoxNzEzMDk5NjAyLCJqdGkiOiJFZUp3cDNNYmlaNCIsImNpZCI6InBsYXktdmFsb3JhbnQtd2ViLXByb2QifQ.PH-SbDiG5-EzeWRXwIEpJPM19bqG3ZuzEXQeF5jMKhADXPddSFADGqoH8PqXE2NtOG-esbvtYM1yjZSBtgV2M3KgijE81oYYm1HTyMkjHKlbqCI3jIGSImNcm1AUvIygnyx7DyHsmOOV29ricTzIEiycTh-1bOut4-O8ctHWmAY";
+    String PUUID = "";
+    int start = 0;
+    int end = 20;
+
     public MatchHistoryResponse getMatches() {
         return matches;
     }
@@ -34,6 +34,7 @@ public class DataScrollerView implements Serializable {
     public void setMatches(MatchHistoryResponse matches) {
         this.matches = matches;
     }
+
     public String getPUUIDFromFlash() {
         return (String) FacesContext.getCurrentInstance().getExternalContext().getFlash().get("puuid");
     }
@@ -45,19 +46,16 @@ public class DataScrollerView implements Serializable {
     private MatchHistoryService service;
     @Inject
     PlayerHistory playerHistory;
+
     @PostConstruct
     public void init() {
-
+        long start = System.currentTimeMillis();
         try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://valorant-api.com/v1/maps"))
-                    .header("User-Agent", "insomnium/0.2.3-a")
-                    .method("GET", HttpRequest.BodyPublishers.noBody())
-                    .build();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://valorant-api.com/v1/maps")).header("User-Agent", "insomnium/0.2.3-a").method("GET", HttpRequest.BodyPublishers.noBody()).build();
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
 
             ObjectMapper mapper = new ObjectMapper();
-            mapsResponse  = mapper.readValue(response.body(), MapsResponse.class);
+            mapsResponse = mapper.readValue(response.body(), MapsResponse.class);
 
             PUUID = getPUUIDFromFlash();
             if (PUUID != null && !PUUID.isEmpty()) {
@@ -66,12 +64,13 @@ public class DataScrollerView implements Serializable {
 
             } else {
                 // Handle missing PUUID, maybe redirect or show error message "eu", PUUID, Authorization, X_Riot_Entitlements_JWT, start, end
-                System.out.println("PUUID is missing or invalid.");
+                //System.out.println("PUUID is missing or invalid.");
             }
         } catch (Exception e) {
             // Log or handle initialization error
             e.printStackTrace();
         }
+        System.out.println("Total execution time : " + (System.currentTimeMillis()-start));
     }
 
 
@@ -83,18 +82,17 @@ public class DataScrollerView implements Serializable {
         this.service = service;
     }
 
-    public String getmorematches() throws IOException, InterruptedException {
-        start=end;
-        end=end+20;
+    public String getmorematches() throws Exception {
+        start = end;
+        end = end + 20;
         matches = service.getMatchHistory(PUUID); // "eu",PUUID,Authorization,X_Riot_Entitlements_JWT,start,end
         return "matches.xhtml?faces-redirect=true";
     }
 
     public String getmapUUID(String mapid) throws ApiException, IOException, InterruptedException {
-        System.out.println(playerHistory.getPuuid());
 
         for (int i = 0; i < mapsResponse.getData().size(); i++) {
-            if (mapsResponse.getData().get(i).getMapUrl().compareTo(mapid) == 0){
+            if (mapsResponse.getData().get(i).getMapUrl().compareTo(mapid) == 0) {
                 return mapsResponse.getData().get(i).getUuid();
             }
         }
--- src/main/java/com/talhakoek/valorant/models/MatchDetailsResponse.java
+++ src/main/java/com/talhakoek/valorant/models/MatchDetailsResponse.java
@@ -6,45 +6,88 @@ import java.util.ArrayList;
 
 public class MatchDetailsResponse {
     @JsonProperty("matchInfo")
-    public MatchInfo getMatchInfo() { 
-		 return this.matchInfo; } 
-    public void setMatchInfo(MatchInfo matchInfo) { 
-		 this.matchInfo = matchInfo; } 
+    public MatchInfo getMatchInfo() {
+        return this.matchInfo;
+    }
+
+    public void setMatchInfo(MatchInfo matchInfo) {
+        this.matchInfo = matchInfo;
+    }
+
     MatchInfo matchInfo;
-    @JsonProperty("players") 
+
+    @JsonProperty("players")
     public ArrayList<Player> getPlayers() {
-		 return this.players; } 
-    public void setPlayers(ArrayList<Player> players) { 
-		 this.players = players; } 
+        return this.players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
     ArrayList<Player> players;
-    @JsonProperty("bots") 
-    public ArrayList<Object> getBots() { 
-		 return this.bots; } 
-    public void setBots(ArrayList<Object> bots) { 
-		 this.bots = bots; } 
+
+    @JsonProperty("bots")
+    public ArrayList<Object> getBots() {
+        return this.bots;
+    }
+
+    public void setBots(ArrayList<Object> bots) {
+        this.bots = bots;
+    }
+
     ArrayList<Object> bots;
-    @JsonProperty("coaches") 
-    public ArrayList<Object> getCoaches() { 
-		 return this.coaches; } 
-    public void setCoaches(ArrayList<Object> coaches) { 
-		 this.coaches = coaches; } 
+
+    @JsonProperty("coaches")
+    public ArrayList<Object> getCoaches() {
+        return this.coaches;
+    }
+
+    public void setCoaches(ArrayList<Object> coaches) {
+        this.coaches = coaches;
+    }
+
     ArrayList<Object> coaches;
-    @JsonProperty("teams") 
-    public ArrayList<Team> getTeams() { 
-		 return this.teams; } 
-    public void setTeams(ArrayList<Team> teams) { 
-		 this.teams = teams; } 
+
+    @JsonProperty("teams")
+    public ArrayList<Team> getTeams() {
+        return this.teams;
+    }
+
+    public void setTeams(ArrayList<Team> teams) {
+        this.teams = teams;
+    }
+
     ArrayList<Team> teams;
-    @JsonProperty("roundResults") 
-    public ArrayList<RoundResult> getRoundResults() { 
-		 return this.roundResults; } 
-    public void setRoundResults(ArrayList<RoundResult> roundResults) { 
-		 this.roundResults = roundResults; } 
+
+    @JsonProperty("roundResults")
+    public ArrayList<RoundResult> getRoundResults() {
+        return this.roundResults;
+    }
+
+    public void setRoundResults(ArrayList<RoundResult> roundResults) {
+        this.roundResults = roundResults;
+    }
+
     ArrayList<RoundResult> roundResults;
-    @JsonProperty("kills") 
-    public ArrayList<Kill> getKills() { 
-		 return this.kills; } 
-    public void setKills(ArrayList<Kill> kills) { 
-		 this.kills = kills; } 
+
+    @JsonProperty("kills")
+    public ArrayList<Kill> getKills() {
+        return this.kills;
+    }
+
+    public void setKills(ArrayList<Kill> kills) {
+        this.kills = kills;
+    }
+
     ArrayList<Kill> kills;
+    MatchResults results;
+
+    public MatchResults getResults() {
+        return results;
+    }
+
+    public void setResults(MatchResults results) {
+        this.results = results;
+    }
 }
