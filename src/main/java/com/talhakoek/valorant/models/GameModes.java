6a045a845146e710fa757f9c113c74af6e25e38b
Talha Kök <talha@talhakoek.com>
2024-04-17 19:42:59 +0000

    Score fixed

---
+++ src/main/java/com/talhakoek/valorant/models/GameModes.java
@@ -0,0 +1,21 @@
+package com.talhakoek.valorant.models;
+
+public class GameModes {
+    String assetpath,UUID;
+
+    public String getAssetpath() {
+        return assetpath;
+    }
+
+    public void setAssetpath(String assetpath) {
+        this.assetpath = assetpath;
+    }
+
+    public String getUUID() {
+        return UUID;
+    }
+
+    public void setUUID(String UUID) {
+        this.UUID = UUID;
+    }
+}
--- src/main/java/com/talhakoek/valorant/services/MatchDetailsService.java
+++ src/main/java/com/talhakoek/valorant/services/MatchDetailsService.java
@@ -23,6 +23,8 @@ import java.util.List;
 @ApplicationScoped
 public class MatchDetailsService {
 
+    List<Maps>
+
     @Inject
     PlayerHistory playerHistory;
 
